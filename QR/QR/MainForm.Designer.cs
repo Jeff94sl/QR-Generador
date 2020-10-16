d709e60ff87d397707befff623595ad308b66cd1
Jefrin Salgado <jeffrynsl@hotmail.es>
2020-10-15 22:29:18 -0600

    Version 1

---
+++ QR/QR/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace QR
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Generar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Abrir = new System.Windows.Forms.OpenFileDialog();
+            this.Logo = new System.Windows.Forms.Button();
+            this.Emergente = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Emergente.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informacion";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(317, 247);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Generar
+            // 
+            this.Generar.Location = new System.Drawing.Point(172, 294);
+            this.Generar.Name = "Generar";
+            this.Generar.Size = new System.Drawing.Size(75, 23);
+            this.Generar.TabIndex = 2;
+            this.Generar.Text = "Generar";
+            this.Generar.UseVisualStyleBackColor = true;
+            this.Generar.Click += new System.EventHandler(this.Generar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.Emergente;
+            this.pictureBox1.Location = new System.Drawing.Point(337, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Logo
+            // 
+            this.Logo.Location = new System.Drawing.Point(91, 294);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(75, 23);
+            this.Logo.TabIndex = 4;
+            this.Logo.Text = "Cargar Logo";
+            this.Logo.UseVisualStyleBackColor = true;
+            this.Logo.Click += new System.EventHandler(this.Logo_Click);
+            // 
+            // Emergente
+            // 
+            this.Emergente.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guardarToolStripMenuItem});
+            this.Emergente.Name = "Emergente";
+            this.Emergente.Size = new System.Drawing.Size(117, 26);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 380);
+            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Generar);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "QR Generador";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Emergente.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Generar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog Abrir;
+        private System.Windows.Forms.Button Logo;
+        private System.Windows.Forms.ContextMenuStrip Emergente;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+    }
+}
+
---
+++ QR/QR/MainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace QR
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        string img = String.Empty;
+        private void Generar_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(richTextBox1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            if (img != "")
+            {
+                qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile(img));
+            }
+            pictureBox1.Image = qrCodeImage;
+        }
+
+        private void Logo_Click(object sender, EventArgs e)
+        {
+            if (Abrir.ShowDialog() == DialogResult.OK)
+            {
+                img = Abrir.FileName;
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Guardar = new SaveFileDialog();
+
+            if (Guardar.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(Guardar.FileName);
+            }
+        }
+    }
+}
